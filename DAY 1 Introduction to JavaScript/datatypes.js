9759e3f2b770659c4e83e9a53a747345d1e34bf1
akashah-09 <officialakashah@gmail.com>
2024-01-15 22:16:43 +0500

    commit

---
+++ DAY 1 Introduction to JavaScript/datatypes.js
@@ -0,0 +1,7 @@
+/* 5- Create datatypes.js file and use the JavaScript typeof operator to check different data types. Check the data type of each variable */
+
+console.log(typeof stringOne);
+console.log(typeof boolOne);
+console.log(typeof unDefOne);
+console.log(typeof nullOne);
+
---
+++ DAY 1 Introduction to JavaScript/variable.js
@@ -0,0 +1,45 @@
+/* 4- Create a variable.js file and declare variables and assign string, boolean, undefined and null data types */
+
+let stringOne = "Computer";
+let boolOne = true;
+let unDefOne;
+let nullOne = null;
+
+/* 6- Declare four variables without assigning values */
+
+let one;
+let two;
+let three;
+let four;
+
+/* 7- Declare four variables with assigned values */
+
+let language = 'JavaScript';
+let challenge = '30DaysOfJavaScript';
+let day = 1;
+let year = 2024;
+
+
+/* 8- Declare variables to store your first name, last name, marital status, country and age in multiple lines */
+let firstName = 'Aka';
+let lastname = 'Shah';
+let maritalStatus = 'divorced';
+let country = 'Pakistan';
+let age = '19';
+
+/* 9- Declare variables to store your first name, last name, marital status, country and age in a single line */
+firstName = 'Aka'; lastname = "Shah"; maritalStatus = 'divorced'; country = 'pakistan'; age = '19';
+
+
+/* 10 - Declare two variables myAge and yourAge and assign them initial values and log to the browser console.
+I am 25 years old.
+You are 30 years old. */
+
+let myAge = 19;
+let yourAge = 30;
+console.log('I am ' + myAge + ' years old.')
+console.log('You are ' + yourAge + ' years old.')
+
+//Using backticks method
+console.log(`I am ${myAge} years old.
+You are ${yourAge} years old.`);
